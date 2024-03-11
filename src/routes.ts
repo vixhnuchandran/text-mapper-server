13ebf5c7733b8ff05756b3bf8228e201e8a11bbd
vixhnuchandran <vixhnuchandran@gmail.com>
2024-03-11 08:32:21 +0530

    feat: Add root route

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,10 @@ import multer from "multer"
 const upload = multer({
     storage: multer.memoryStorage(),
 })
+routes.get("/", (req, res) => {
+    console.log("Text Mapper Server")
+    res.status(200).send("Text Mapper Server")
+})
 
 routes.post("/ocr-service", upload.single("image"), routeHandler.ocrService)
 
