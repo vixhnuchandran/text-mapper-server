e87abd68eeb2baad27f6fc312680e1554fd0cfa6
vixhnuchandran <vixhnuchandran@gmail.com>
2024-03-12 21:04:37 +0530

    refactor: Simplify OCR service in handler.js
    -  Removed unnecessary imports: The imports for loadImage, createCanvas, and CanvasRenderingContext2D from the canvas package were removed since they were not used anymore.
    -  Removed canvas-related operations: The usage of canvas and canvas-related operations for drawing bounding boxes around recognized words was removed. Instead, the bounding box data directly extracted from the Tesseract result is sent in the response.
    -  Updated Tesseract usage: The Tesseract.recognize function is now used directly to recognize text from the image buffer. The result is processed to extract both the recognized text and the bounding boxes of individual words.
    - Simplified response: The response now includes only the extracted text and the bounding box data, eliminating the unnecessary processing of the image and the generation of processed images in base64 format.

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,5 @@
 import { Request, Response, NextFunction, Router } from "express"
-import { StatusCodes as httpCodes } from "http-status-codes"
 import Tesseract from "tesseract.js"
-import { loadImage, createCanvas, CanvasRenderingContext2D } from "canvas"
 
 const ocrService = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -13,66 +11,26 @@ const ocrService = async (req: Request, res: Response, next: NextFunction) => {
         const imgBuffer = req.file?.buffer
         const imageName = req.file?.originalname
 
-        const img = await loadImage(imgBuffer)
-        const canvas = createCanvas(img.width, img.height)
-        const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+        const resultData = await Tesseract.recognize(imgBuffer, "eng")
 
-        ctx.drawImage(img, 0, 0)
-
-        const {
-            data: { words, lines },
-        } = await Tesseract.recognize(imgBuffer, "eng")
-
-        const data = { words, lines }
-
-        drawBoundingBoxes(ctx, data.words)
-
-        const extractedText = words.map(word => word.text).join(" ")
-
-        const dataUrl = canvas.toDataURL("image/png")
-        const byteString = atob(dataUrl.split(",")[1])
-        const mimeString = dataUrl.split(",")[0].split(":")[1].split(";")[0]
-        const ab = new ArrayBuffer(byteString.length)
-        const ia = new Uint8Array(ab)
-        for (let i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i)
-        }
-
-        const blob = new Blob([ab], { type: mimeString })
-
-        const file = new File([blob], "processed_image.png")
-        const processedImageBuffer = await convertFileToBuffer(file)
-
-        const base64Data = processedImageBuffer.toString("base64")
+        const extractedBboxes = resultData.data.words.map(word => ({
+            [word.text]: word.bbox,
+        }))
+        const extractedText = resultData.data.words
+            .map(word => word.text)
+            .join(" ")
 
         console.log("extractedText: ", extractedText)
-        console.log("processedImage: ", base64Data)
+        console.log("extractedBboxes: ", extractedBboxes)
 
         res.setHeader("Content-Type", "application/json")
         res.status(200).json({
             extractedText,
-            processedImage: base64Data,
+            extractedBboxes,
         })
     } catch (error) {
         console.error("Error:", error)
         res.status(500).json({ error: "Failed to process image" })
-    }
-}
-
-const convertFileToBuffer = async (file: File) => {
-    return Buffer.from(await file.arrayBuffer())
-}
-
-const drawBoundingBoxes = (
-    ctx: CanvasRenderingContext2D,
-    words: Tesseract.Word[]
-) => {
-    ctx.strokeStyle = "red"
-    ctx.lineWidth = 2
-
-    for (const word of words) {
-        const { x0, y0, x1, y1 } = word.bbox
-        ctx.strokeRect(x0, y0, x1 - x0, y1 - y0)
     }
 }
 
